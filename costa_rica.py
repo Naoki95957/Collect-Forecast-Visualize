2e39dc6d2ff91fc83ab1c29361b4bf686a2e6902
Vitaliy Stepanov <vitaliybeinspired@gmail.com>
2020-11-04 19:00:09 -0800

    refactored

--- costa_rica.py
+++ costa_rica.py
@@ -19,13 +19,14 @@ driver.get(URL)
 
 
 def scrape_date(date="") -> list:
-    if not bool(date):
-        date = str(datetime.date.today() - timedelta(days=1))
-
-    driver.find_element_by_name("formPosdespacho:txtFechaInicio_input").send_keys(Keys.RETURN)
+    if not bool(date):  # get yesterdays data by default
+        yesterday = datetime.date.today() - timedelta(days=1)
+        date = str(yesterday.day).zfill(2) + "/" + str(yesterday.month).zfill(2) + "/" + str(yesterday.year).zfill(4)
+    search_date = driver.find_element_by_name("formPosdespacho:txtFechaInicio_input")
+    search_date.clear()
+    search_date.send_keys(date + Keys.RETURN)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
-
     cells = soup.find('tbody', {'id': 'formPosdespacho:j_id_1a_data'}).find_all('span')
     list = []
     for cell in cells:
@@ -34,12 +35,12 @@ def scrape_date(date="") -> list:
     return list
 
 
-def get_data_point(date: str, data: BeautifulSoup) -> dict:
-    location = re.search(r'(.*?),(.*)', data['title']).group(1)
-    time = re.search(r'(.*?),(.*)', data['title']).group(2)
-    time_stamp = arrow.get(date + time, 'YYYY-MM-DD HH:mm', locale="es", tzinfo='America/Costa_Rica').strftime("%Y-%-m-%-d %H:%M")
-    return {'ts': time_stamp,
-            'value': data.getText(),
+def get_data_point(date: str, soup: BeautifulSoup) -> dict:
+    location = re.search(r'(.*?),(.*)', soup['title']).group(1)
+    time = re.search(r'(.*?),(.*)', soup['title']).group(2)
+    date_time = arrow.get(date + time, 'DD/MM/YYYY HH:mm', locale="es", tzinfo='America/Costa_Rica').datetime
+    return {'ts': date_time,
+            'value': float(soup.getText()),
             'ba': BA,
             'meta': location + " (MWh)"}
 
