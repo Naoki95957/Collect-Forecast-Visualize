7b88c05df342571c703f4d7286f5a719896aa075
Vitaliy Stepanov <vitaliybeinspired@gmail.com>
2020-11-04 17:21:03 -0800

    refactor

--- costa_rica.py
+++ costa_rica.py
@@ -1,7 +1,7 @@
 import selenium
 import re
 import datetime
-import dateutil
+from datetime import timedelta
 import arrow
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -18,53 +18,34 @@ driver = selenium.webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get(URL)
 
 
-def costaRicaScraper(date="") -> list:
+def scrape_date(date="") -> list:
     if not bool(date):
-        todays_date = datetime.date.today()
-        date = str(todays_date.day).zfill(2) + "/" + str(todays_date.month).zfill(2) + "/" + str(
-            todays_date.year).zfill(4)
-    # clear the field and put in today's date
-    input_field = driver.find_element_by_name("formPosdespacho:txtFechaInicio_input")
-    input_field.clear()
-    input_field.send_keys(date + Keys.RETURN)
+        date = str(datetime.date.today() - timedelta(days=1))
 
+    driver.find_element_by_name("formPosdespacho:txtFechaInicio_input").send_keys(Keys.RETURN)
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    cells = soup.find('tbody', {'id': 'formPosdespacho:j_id_1a_data'}).find_all('span')
-
-    output_list = []
-    for cell in cells:
-        if cell.has_attr('title') and bool(cell.getText()):
-            if 'Total' not in cell['title']:
-                output_list.append(formatter(date, cell))
     driver.quit()
-    return output_list
+
+    cells = soup.find('tbody', {'id': 'formPosdespacho:j_id_1a_data'}).find_all('span')
+    list = []
+    for cell in cells:
+        if cell.has_attr('title') and bool(cell.getText()) and 'Total' not in cell['title']:
+            list.append(get_data_point(date, cell))
+    return list
 
 
-def formatter(todaysDate: str, data: BeautifulSoup) -> dict:
-    # parse out the location and hour of each cell
+def get_data_point(date: str, data: BeautifulSoup) -> dict:
     location = re.search(r'(.*?),(.*)', data['title']).group(1)
     time = re.search(r'(.*?),(.*)', data['title']).group(2)
-    return {'ts': arrow.get(todaysDate + time, 'DD/MM/YYYY HH:mm').datetime,
+    time_stamp = arrow.get(date + time, 'YYYY-MM-DD HH:mm', locale="es", tzinfo='America/Costa_Rica').strftime("%Y-%-m-%-d %H:%M")
+    return {'ts': time_stamp,
             'value': data.getText(),
             'ba': BA,
             'meta': location + " (MWh)"}
 
-"""
-    spanish_time = soup.find('div', {'class': 'sitr-update'}).find_all('span')[0].get_text()
-    time_stamp_sp = arrow.get(spanish_time, 'DD-MMMM-YYYY H:mm:ss', locale="es", tzinfo="America/Panama")
-    time_stamp_en = time_stamp_sp.datetime.strftime("%Y-%-m-%-d %H:%M:%S")
-
-
-datapoint1 = \
- {'ts': <timezone aware datetime object>,
-  'value': <value of grid parameter (type float or int)>,
-  'ba': <string specifying balancing authority or subregion eg 'PJM_WEST', 'NL' for Netherlands, 'ISONE_VERMONT'>,
-  'meta': <optional field specifying fuel type (if generation) or other information
-"""
-
 
 def main():
-    for datapoint in costaRicaScraper():
+    for datapoint in scrape_date():
         print(datapoint)
 
 
