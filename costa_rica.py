331b330bedcc0ae0a2395601f5194cfc9e9503aa
Vitaliy Stepanov <vitaliybeinspired@gmail.com>
2020-11-05 17:49:40 -0800

    seperated functions added linux driver added docstring added instructions

--- costa_rica.py
+++ costa_rica.py
@@ -7,46 +7,66 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-# change drivers depending on OS and Web Browser (Mac and Chrome default)
-DRIVER_PATH = './drivers/chromedriver'
-URL = 'https://apps.grupoice.com/CenceWeb/CencePosdespachoNacional.jsf'
-BA = 'Operación Sistema Eléctrico Nacional'
+"""
+Retrives emission data as a list of dictionaries from Costa Rica by hours for each plant
+and balance authority name.
 
+IMPORTANT: Before running, select chromedriver for your OS.
+For MAC, you may get a warning:
+    “mac_chromedriver86” can’t be opened because the identity of the developer cannot be confirmed."
+    Go to Apple > System Preferences > Security & Privacy and click the 'Open Anyway' button.
+"""
+
+SELECT_OS_CHROME_DRIVER = './drivers/mac_chromedriver86'
 options = Options()
 options.headless = True
-driver = selenium.webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get(URL)
+driver = selenium.webdriver.Chrome(options=options, executable_path=SELECT_OS_CHROME_DRIVER)
+costa_rica_URL = 'https://apps.grupoice.com/CenceWeb/CencePosdespachoNacional.jsf'
+driver.get(costa_rica_URL)
 
 
-def scrape_date(date="") -> list:
-    if not bool(date):  # get yesterdays data by default
+def search_date(date=""):
+    """
+    :param date: Enter 'DD/MM/YYYY' to retrieve data from other dates. If empty, yesterday is default
+    :return: Reformatted datetime date to match costa ricas search date field 'DD/MM/YYYY' as a string
+    """
+    if not bool(date):
         yesterday = datetime.date.today() - timedelta(days=1)
         date = str(yesterday.day).zfill(2) + "/" + str(yesterday.month).zfill(2) + "/" + str(yesterday.year).zfill(4)
-    search_date = driver.find_element_by_name("formPosdespacho:txtFechaInicio_input")
-    search_date.clear()
-    search_date.send_keys(date + Keys.RETURN)
+    search_date_field = driver.find_element_by_name("formPosdespacho:txtFechaInicio_input")
+    search_date_field.clear()
+    search_date_field.send_keys(date + Keys.RETURN)
+    return date
+
+
+def scrape_data(date) -> list:
+    """
+    :return: A list of data_points dictionaries
+    """
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
-    cells = soup.find('tbody', {'id': 'formPosdespacho:j_id_1a_data'}).find_all('span')
-    list = []
-    for cell in cells:
-        if cell.has_attr('title') and bool(cell.getText()) and 'Total' not in cell['title']:
-            list.append(get_data_point(date, cell))
-    return list
+    plants_hours = soup.find('tbody', {'id': 'formPosdespacho:j_id_1a_data'}).find_all('span')
+    data_points = []
+    for plant_hour in plants_hours:
+        if plant_hour.has_attr('title') and bool(plant_hour.getText()) and 'Total' not in plant_hour['title']:
+            data_points.append(data_point(date, plant_hour))
+    return data_points
 
 
-def get_data_point(date: str, soup: BeautifulSoup) -> dict:
-    location = re.search(r'(.*?),(.*)', soup['title']).group(1)
-    time = re.search(r'(.*?),(.*)', soup['title']).group(2)
-    date_time = arrow.get(date + time, 'DD/MM/YYYY HH:mm', locale="es", tzinfo='America/Costa_Rica').datetime
-    return {'ts': date_time,
-            'value': float(soup.getText()),
-            'ba': BA,
-            'meta': location + " (MWh)"}
+def data_point(date, plant_hour) -> dict:
+    plant = re.search(r'(.*?),(.*)', plant_hour['title']).group(1)
+    emission_hour = re.search(r'(.*?),(.*)', plant_hour['title']).group(2)
+    time_stamp = arrow.get(date + emission_hour, 'DD/MM/YYYY HH:mm', locale="es", tzinfo='America/Costa_Rica').datetime
+    return {'ts': time_stamp,
+            'value': float(plant_hour.getText()),
+            'ba': 'Operación Sistema Eléctrico Nacional',
+            'meta': plant + " (MWh)"}
 
 
 def main():
-    for datapoint in scrape_date():
+    date = search_date()
+    data_points = scrape_data(date)
+    for datapoint in data_points:
         print(datapoint)
 
 
