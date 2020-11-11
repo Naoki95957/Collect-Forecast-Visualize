41d1c55e852384078f25eab4b419c0686916fb48
Max Ayala <maxcayala@gmail.com>
2020-11-11 14:36:08 -0800

    Refactor into class

--- el_salvador.py
+++ el_salvador.py
@@ -7,134 +7,137 @@ import arrow
 import re
 from bs4 import BeautifulSoup
 
-URL = "http://estadistico.ut.com.sv/OperacionDiaria.aspx"
+class ElSalvador:
 
-def scraper() -> list:
-    """
-    Create a request to the URL defined above. This then takes the hidden keys 
-    and event triggers to make a second request with the encoded data.
+    URL = "http://estadistico.ut.com.sv/OperacionDiaria.aspx"
 
-    The response is actually just encoded data to be loaded into the page.
-    
-    Luckily for us, it is encoded in JSON and is easily parsible.
+    def scrape(self) -> list:
+        """
+        Create a request to the URL defined above. This then takes the hidden keys 
+        and event triggers to make a second request with the encoded data.
 
-    Returns an array of datapoints
-    """
-    firstpage = requests.get(URL)
-    soup = BeautifulSoup(firstpage.content, "html5lib")
-    viewstate = soup.find_all("input", {"type": "hidden", "name": "__VIEWSTATE"})[0]['value']
-    viewgenerator = soup.find_all("input", {"type": "hidden", "name": "__VIEWSTATEGENERATOR"})[0]['value']
-    validation = soup.find_all("input", {"type": "hidden", "name": "__EVENTVALIDATION"})[0]['value']
+        The response is actually just encoded data to be loaded into the page.
+        
+        Luckily for us, it is encoded in JSON and is easily parsible.
 
-    #this is our form data, largely observed by copying what the browser is doing via a packet sniffer
-    formData = {
-        '__EVENTTARGET': '',
-        '__EVENTARGUMENT': '',
-        '__VIEWSTATE': str(viewstate),
-        '__VIEWSTATEGENERATOR': str(viewgenerator),
-        'DXScript': '1_247,1_138,1_241,1_164,1_141,1_135,1_181,24_392,24_391,24_393,24_394,24_397,24_398,24_399,24_395,24_396,15_16,24_379,15_14,15_10,15_11,15_13',
-        'DXCss': '1_28,1_29,24_359,24_364,24_360,15_0,15_4',
-        '__CALLBACKID': 'ASPxDashboardViewer1',
-        '__CALLBACKPARAM': 'c0:{"Task":"Initialize","DashboardId":"OperacionDiaria","Settings":{"calculateHiddenTotals":false},"RequestMarker":0,"ClientState":{}}',
-        '__EVENTVALIDATION': str(validation),
-    }
+        Returns an array of datapoints
+        """
+        firstpage = requests.get(self.URL)
+        soup = BeautifulSoup(firstpage.content, "html5lib")
+        viewstate = soup.find_all("input", {"type": "hidden", "name": "__VIEWSTATE"})[0]['value']
+        viewgenerator = soup.find_all("input", {"type": "hidden", "name": "__VIEWSTATEGENERATOR"})[0]['value']
+        validation = soup.find_all("input", {"type": "hidden", "name": "__EVENTVALIDATION"})[0]['value']
 
-    #encode parameters to the URL
-    encodedData = urllib.parse.urlencode(formData)
-    encodedData = encodedData.encode('ascii')
-    response = urllib.request.urlopen(URL, encodedData)
-    
-    #decode response
-    responseContent = response.read()
-    encoding = response.headers.get_content_charset('utf-8')
-    responseTxt = responseContent.decode(encoding)
+        #this is our form data, largely observed by copying what the browser is doing via a packet sniffer
+        formData = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': str(viewstate),
+            '__VIEWSTATEGENERATOR': str(viewgenerator),
+            'DXScript': '1_247,1_138,1_241,1_164,1_141,1_135,1_181,24_392,24_391,24_393,24_394,24_397,24_398,24_399,24_395,24_396,15_16,24_379,15_14,15_10,15_11,15_13',
+            'DXCss': '1_28,1_29,24_359,24_364,24_360,15_0,15_4',
+            '__CALLBACKID': 'ASPxDashboardViewer1',
+            '__CALLBACKPARAM': 'c0:{"Task":"Initialize","DashboardId":"OperacionDiaria","Settings":{"calculateHiddenTotals":false},"RequestMarker":0,"ClientState":{}}',
+            '__EVENTVALIDATION': str(validation),
+        }
 
-    #had to do manual replacing since decoding it doesn't completely work ^^
-    responseTxt = responseTxt.replace("\\t", "\t")
-    responseTxt = responseTxt.replace("\\n", "\n")
-    responseTxt = responseTxt.replace("\\'", "'")
-    responseTxt = responseTxt.replace("\'", "\"")
+        #encode parameters to the URL
+        encodedData = urllib.parse.urlencode(formData)
+        encodedData = encodedData.encode('ascii')
+        response = urllib.request.urlopen(self.URL, encodedData)
+        
+        #decode response
+        responseContent = response.read()
+        encoding = response.headers.get_content_charset('utf-8')
+        responseTxt = responseContent.decode(encoding)
 
-    #cleans up the junk in the response to get just the JSON part
-    responseTxt = responseTxt.split('\n', 1)[1]
-    responseTxt = responseTxt[:responseTxt.rfind('\n')]
-    responseTxt = "{" + responseTxt + "}"
-    #yes, this monster is back but because python can't perfectly parse JSON. So 'new Date' is now instead a string
-    responseTxt = re.sub(r'(new Date\((\d+,)+\d\))', '\"some date\"', responseTxt)
+        #had to do manual replacing since decoding it doesn't completely work ^^
+        responseTxt = responseTxt.replace("\\t", "\t")
+        responseTxt = responseTxt.replace("\\n", "\n")
+        responseTxt = responseTxt.replace("\\'", "'")
+        responseTxt = responseTxt.replace("\'", "\"")
 
-    #writing file to see it for now
-    #file = open("output.txt", "w")
-    #file.write(responseTxt)
-    #file.close()
+        #cleans up the junk in the response to get just the JSON part
+        responseTxt = responseTxt.split('\n', 1)[1]
+        responseTxt = responseTxt[:responseTxt.rfind('\n')]
+        responseTxt = "{" + responseTxt + "}"
+        #yes, this monster is back but because python can't perfectly parse JSON. So 'new Date' is now instead a string
+        responseTxt = re.sub(r'(new Date\((\d+,)+\d\))', '\"some date\"', responseTxt)
 
-    #parse json and fetch our specific bits of data
-    jsonObj = json.loads(responseTxt)
-    #table data
-    data = jsonObj['PaneContent'][0]['ItemData']['DataStorageDTO']['Slices'][1]['Data']
-    #current day
-    currentDay = jsonObj['DashboardParameters'][0]['Values'][0]['DisplayText']
-    currentDay = re.sub(" 12:00:00 a.m.", "", currentDay)
+        #writing file to see it for now
+        #file = open("output.txt", "w")
+        #file.write(responseTxt)
+        #file.close()
 
-    #the data is weird...
-    #[x, y, z]:{"0":data}
-    #x is column
-    #y ???
-    #z is row : by hour of day
-    #data: the numbers we're after (MWh)
-    #they don't even have their indexing correct!!!! :( 
-    #the table goes: ... interconnection, solar, thermal
-    #the index goes: ... interconnection, thermal, solar
-    columnIdentifier = ["Biomass", "Geothermal", "HydroElectric", "Interconnection", "Thermal", "Solar"]
-    listOfDatapoints = []
-    for entry in data:
-        column = int(re.search(r'\[(\d+),\d+,\d+\]', entry).group(1))
-        row = int(re.search(r'\[\d+,\d+,(\d+)\]', entry).group(1))
-        value = data[entry]["0"]
-        hour = str(row).zfill(2) + ":00"
-        date = arrow.get(currentDay + hour, 'DD/MM/YYYYHH:mm', locale="es", tzinfo=dateutil.tz.gettz('America/El_Salvador')).datetime
-        datapoint = formatter(columnIdentifier[column], date, value)
-        listOfDatapoints.append(datapoint)
-    return listOfDatapoints
+        #parse json and fetch our specific bits of data
+        jsonObj = json.loads(responseTxt)
+        #table data
+        data = jsonObj['PaneContent'][0]['ItemData']['DataStorageDTO']['Slices'][1]['Data']
+        #current day
+        currentDay = jsonObj['DashboardParameters'][0]['Values'][0]['DisplayText']
+        currentDay = re.sub(" 12:00:00 a.m.", "", currentDay)
 
-def formatter(columnName: str, dateTime: datetime.date, value: float) -> dict:
-    """
-    Takes column, hour, and value from the scraper and prepares it as a datapoint
+        #the data is weird...
+        #[x, y, z]:{"0":data}
+        #x is column
+        #y ???
+        #z is row : by hour of day
+        #data: the numbers we're after (MWh)
+        #they don't even have their indexing correct!!!! :( 
+        #the table goes: ... interconnection, solar, thermal
+        #the index goes: ... interconnection, thermal, solar
+        columnIdentifier = ["Biomass", "Geothermal", "HydroElectric", "Interconnection", "Thermal", "Solar"]
+        listOfDatapoints = []
+        for entry in data:
+            column = int(re.search(r'\[(\d+),\d+,\d+\]', entry).group(1))
+            row = int(re.search(r'\[\d+,\d+,(\d+)\]', entry).group(1))
+            value = data[entry]["0"]
+            hour = str(row).zfill(2) + ":00"
+            date = arrow.get(currentDay + hour, 'DD/MM/YYYYHH:mm', locale="es", tzinfo=dateutil.tz.gettz('America/El_Salvador')).datetime
+            datapoint = self.formatter(columnIdentifier[column], date, value)
+            listOfDatapoints.append(datapoint)
+        return listOfDatapoints
 
-    The day should always be the current day and El-Salvador's time zone is:
-    Central Standard Time (GMT-6)
+    def formatter(self, columnName: str, dateTime: datetime.date, value: float) -> dict:
+        """
+        Takes column, hour, and value from the scraper and prepares it as a datapoint
 
-    Their reports show up an hour late.
+        The day should always be the current day and El-Salvador's time zone is:
+        Central Standard Time (GMT-6)
 
-    Should return a single dictionary in the proper form WattTime requested
+        Their reports show up an hour late.
 
-    Parameters:
+        Should return a single dictionary in the proper form WattTime requested
 
-    columnName -- str that simple has the name of the production
-    
-    hour -- int that represents the hour in a 24hr day
+        Parameters:
 
-    value -- a decimal value that represents the electricty produced in MWh
-    """
-    #datapoint1 = 
-    #{
-    #    'ts': <timezone aware datetime object>,
-    #    'value': <value of grid parameter (type float or int)>,
-    #    'ba': <string specifying balancing authority or subregion eg 'PJM_WEST', 'NL' for Netherlands, 'ISONE_VERMONT'>,
-    #    'meta': <optional field specifying fuel type (if generation) or other information needed to make sense of the particular data point>
-    #}
-    # So the returned data looks something like: [datapoint1, datapoint2, datapoint3 ....]
-    # Our BA is: Unidad de Transacciones (UT)
-    # Transactions Unit (Unidad de Transacciones)
+        columnName -- str that simple has the name of the production
+        
+        hour -- int that represents the hour in a 24hr day
 
-    datapoint = {}
-    datapoint['ts'] = dateTime
-    datapoint['value'] = value
-    datapoint['ba'] = "Unidad de Transacciones"
-    datapoint['meta'] = columnName + " (MWh)"
-    return datapoint
+        value -- a decimal value that represents the electricty produced in MWh
+        """
+        #datapoint1 = 
+        #{
+        #    'ts': <timezone aware datetime object>,
+        #    'value': <value of grid parameter (type float or int)>,
+        #    'ba': <string specifying balancing authority or subregion eg 'PJM_WEST', 'NL' for Netherlands, 'ISONE_VERMONT'>,
+        #    'meta': <optional field specifying fuel type (if generation) or other information needed to make sense of the particular data point>
+        #}
+        # So the returned data looks something like: [datapoint1, datapoint2, datapoint3 ....]
+        # Our BA is: Unidad de Transacciones (UT)
+        # Transactions Unit (Unidad de Transacciones)
+
+        datapoint = {}
+        datapoint['ts'] = dateTime
+        datapoint['value'] = value
+        datapoint['ba'] = "Unidad de Transacciones"
+        datapoint['meta'] = columnName + " (MWh)"
+        return datapoint
 
 def main():
-    for datapoint in scraper():
+    scraper = ElSalvador()
+    for datapoint in scraper.scrape():
         print(datapoint)
 
 if __name__ == "__main__":
