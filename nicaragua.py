31fc75003d7b69fbcc1786e1f6672e728c7f21da
Andre Weertman <aweertman@comcast.net>
2020-11-11 18:48:51 -0800

    made nicaragua flake8 compliant

--- nicaragua.py
+++ nicaragua.py
@@ -3,13 +3,8 @@ from datetime import timedelta
 import dateutil
 import arrow
 import platform
-import re
 from bs4 import BeautifulSoup
 import selenium
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,7 +13,9 @@ from selenium.webdriver.common.by import By
 
 class Nicaragua:
 
-    URL = 'http://www.cndc.org.ni/consultas/reportesDiarios/postDespachoEnergia.php?fecha='
+    URL = ('http://www.cndc.org.ni/'
+           'consultas/reportesDiarios/'
+           'postDespachoEnergia.php?fecha=')
     BA = 'Centro Nacional de Despacho de Carga'
     driver = None
 
@@ -58,10 +55,15 @@ class Nicaragua:
         self.driver.get(url)
 
         timeout = 10
-        WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.XPATH, '//div[@id="Postdespacho"]//table[@id="GeneracionXAgente"]')))
+        WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((
+                By.XPATH, ('//div[@id="Postdespacho"]/'
+                           '/table[@id="GeneracionXAgente"]'))))
 
-        tableDate = self.driver.find_element_by_id('dtpFechaConsulta').get_attribute('value')
-        tabs = self.driver.find_element_by_class_name('tabs').find_elements_by_tag_name('table')
+        tableDate = self.driver.find_element_by_id(
+            'dtpFechaConsulta').get_attribute('value')
+        tabs = self.driver.find_element_by_class_name(
+            'tabs').find_elements_by_tag_name('table')
         tabs[1].click()
 
         return self.__scrape_data(self.driver, date, tableDate)
@@ -70,34 +72,37 @@ class Nicaragua:
         soup = BeautifulSoup(driver.page_source, "html5lib")
 
         tab = soup.find('div', {'id': 'Postdespacho'})
-        plants_hours = tab.find('table', {'id': 'GeneracionXAgente'}).findAll('tr')
+        plants_hours = tab.find(
+            'table', {'id': 'GeneracionXAgente'}).findAll('tr')
         headerCells = plants_hours[1].findAll('td')
 
         data_points_list = []
         for row in range(2, len(plants_hours)):
             rowEntry = plants_hours[row].findAll('td')
-        #data starts on 1 since 0 is just the hour, skip last since it's just total
         for column in range(1, len(rowEntry) - 1):
             value = rowEntry[column].getText()
             header = headerCells[column - 1].getText()
             hour = rowEntry[0].getText()
-            #if value is empty, put a 0
+            # if value is empty, put a 0
             if not bool(value):
                 value = '0'
-            data_points_list.append(self.__data_point(header, tableDate, hour, value))
+            data_points_list.append(
+                self.__data_point(header, tableDate, hour, value))
 
         return data_points_list
 
-
-    def __data_point(self, location: str, todaysDate: str, hour: int, value: float) -> dict:
+    def __data_point(self, location, todaysDate, hour, value) -> dict:
         datapoint = {}
         datapoint['ts'] = arrow.get(todaysDate + str(hour).zfill(2) + ":00",
-            'DD/MM/YYYYHH:mm',
-            locale="es", tzinfo=dateutil.tz.gettz('America/Managua')).datetime
+                                    'DD/MM/YYYYHH:mm',
+                                    locale="es",
+                                    tzinfo=dateutil.tz.gettz(
+                                        'America/Managua')).datetime
         datapoint['value'] = value
         datapoint['ba'] = Nicaragua.BA
         datapoint['meta'] = location + " (MWh)"
         return datapoint
+
 
 def main():
     print("Initializing driver...")
@@ -112,6 +117,7 @@ def main():
     other_date_data = nicaragua.search_date('03/11/2020')
     for datapoint in other_date_data:
         print(datapoint)
+
 
 if __name__ == "__main__":
     main()
