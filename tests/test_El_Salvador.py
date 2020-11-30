11b352ab4ea0700465604453081c85c5acd69c65
Naoki95957 <naoki.l.95957@gmail.com>
2020-11-30 15:52:25 -0800

    removed print statements

--- tests/test_El_Salvador.py
+++ tests/test_El_Salvador.py
@@ -10,7 +10,6 @@ def get_driver():
 
 
 def test_driver_crash():
-    print("Testing driver")
     try:
         ElSalvador()
         assert True
@@ -20,7 +19,6 @@ def test_driver_crash():
 
 
 def test_multiple_queries(get_driver):
-    print("Testing queries")
     es = get_driver
     try:
         es.date(10, 10, 2020)
@@ -32,35 +30,30 @@ def test_multiple_queries(get_driver):
 
 
 def test_output_list(get_driver):
-    print("Testing if output is list")
     es = get_driver
     data = es.date(10, 10, 2020)
     assert list == type(data)
 
 
 def test_output_value(get_driver):
-    print("Testing if output value is a float")
     es = get_driver
     data = es.date(10, 10, 2020)
     assert isinstance(data[0]['value'], float)
 
 
 def test_output_dict_size(get_driver):
-    print("Testing if output dictionary is size 4")
     es = get_driver
     data = es.date(10, 10, 2020)
     assert 4 == len(data[0])
 
 
 def test_output_tz_aware(get_driver):
-    print("Testing if output is tz aware")
     es = get_driver
     data = es.date(10, 10, 2020)
     assert data[0]['ts'].tzinfo is not None
 
 
 def test_error_future(get_driver):
-    print("Testing for future date error")
     es = get_driver
     try:
         data1 = es.today()
@@ -74,7 +67,6 @@ def test_error_future(get_driver):
 
 
 def test_error_illegal_date(get_driver):
-    print("Testing for illegal date error")
     es = get_driver
     try:
         es.date(40, 10, 2040)
@@ -85,7 +77,6 @@ def test_error_illegal_date(get_driver):
 
 
 def test_error_backwards_dates(get_driver):
-    print("Testing for swaped date range error")
     es = get_driver
     try:
         es.date_range(
