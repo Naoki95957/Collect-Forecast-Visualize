f57b69b6c0556b365bdd5edec7671706cd1fcabe
Naoki Lucas <naoki@iointel.com>
2021-01-19 13:29:20 -0800

    flake8 lint on tester

--- tests/test_El_Salvador_Adapter.py
+++ tests/test_El_Salvador_Adapter.py
@@ -1,11 +1,13 @@
 from adapters.el_salvador_adaptor import ElSalvadorAdapter
 import pytest
 
+
 @pytest.fixture(scope='module', autouse=True)
 def get_adapter():
     print("init el salvador adapter testing")
     esa = ElSalvadorAdapter()
     yield esa
+
 
 def test_adapter_crash():
     try:
