40e579d655c194a19f273888aeacb43326273d19
Naoki Lucas <naoki@iointel.com>
2021-01-26 15:31:39 -0800

    adding threading task

---
+++ AdapterTasks.py
@@ -0,0 +1,26 @@
+from adapters.scraper_adapter import ScraperAdapter
+import threading
+
+class AdapterThread(threading.Thread):
+    '''
+    Automatically handle tasks for simultanious scraping
+    '''
+
+    def __init__(self, scraper: ScraperAdapter):
+        super(AdapterThread, self).__init__()
+        self.scraper = ScraperAdapter
+
+    def start(self):
+        # TODO sort of init
+        return super().start()
+
+    def run(self):
+        # TODO build out running task
+        
+        return super().run()
+
+    # overrides join
+    def join(self, timeout=30):
+        # TODO things to kill running process
+        super().join(timeout)
+
