4f2a2b90efaa65015abe7cd1364fa45bc1463b9d
Naoki Lucas <naoki@iointel.com>
2021-02-02 15:30:24 -0800

    included examples of how we uploaded historic data

--- manager.py
+++ manager.py
@@ -1,9 +1,11 @@
-from adapters.el_salvador_adaptor import ElSalvadorAdapter
+from datetime import datetime
+from adapters.el_salvador_adapter import ElSalvadorAdapter
+from adapters.mexico_adapter import MexicoAdapter
 from adapters.scraper_adapter import ScraperAdapter
-# import datetime
+import datetime
 # import pytz
-# import arrow
-# import pymongo
+import arrow
+import pymongo
 
 # this would be the interface between the DB and python
 # adapters would be the interface between THIS and the scrapers
@@ -13,37 +15,61 @@ from adapters.scraper_adapter import ScraperAdapter
 # connection to the mongodb here
 # use other classes
 
-# client = pymongo.MongoClient(
-#     "mongodb+srv://BCWATT:WattTime2021" +
-#     "@cluster0.tbh2o.mongodb.net/" +
-#     "WattTime?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://BCWATT:WattTime2021" +
+    "@cluster0.tbh2o.mongodb.net/" +
+    "WattTime?retryWrites=true&w=majority")
 
 
-# db = client.get_database('WattTime')
+db = client.get_database('El_Salvador')['Historic']
 
 
-def es_adapter_demo():
-    esa = ElSalvadorAdapter()
-    print(isinstance(esa, ScraperAdapter))
-    # expectation is that 1 might have data
-    print(esa.scrape_new_data())
-    # and that the rest will not due to not waiting
-    print(esa.scrape_new_data())
-    # test_date = arrow.get(
-    #     '02-26/01/2021',
-    #     'HH-DD/MM/YYYY',
-    #     locale='es',
-    #     tzinfo=pytz.timezone('America/El_Salvador')).datetime
-    # esa.set_last_scraped_date(test_date)
-    print(esa.scrape_new_data())
-    print(esa.scrape_new_data())
-    print("flush")
-    data = esa.scrape_history(2021, 1, 18, 2021, 1, 19)
-    for i in data:
-        print(i)
-        for j in data[i]:
-            print('\t', j)
-
+def es_adapter_demo():    
+    el_salvador = ElSalvadorAdapter()
+    start = datetime.date(2019, 1, 1)
+    delta = datetime.timedelta(days=6)
+    
+    for week in range(105):
+        end = start + delta
+        id = start.strftime("%d/%m/%Y")
+        print("Current:")
+        print("\t", id)
+        data = el_salvador.scrape_history(start.year, start.month, start.day, end.year, end.month, end.day)
+        data['_id'] = id
+        #dd/mm/yyyy
+        # increment
+        db.insert_one(data)
+        start = end + datetime.timedelta(days=1)
+        
+    print("Completed to:")
+    print("\t", start.strftime("%d/%m/%Y"))
+    
+def mexico_adapter_demo():    
+    ma = MexicoAdapter()
+    start = datetime.date(2019, 1, 1)
+    delta = datetime.timedelta(days=6)
+    
+    for week in range(105):
+        end = start + delta
+        id = start.strftime("%d/%m/%Y")
+        print("Current:")
+        print("\t", id)
+        data = ma.scrape_history(start.year, start.month, start.day, end.year, end.month, end.day)
+        data['_id'] = id
+        print_data(data)
+        db.insert_one(data)
+        #dd/mm/yyyy
+        # increment
+        start = end + datetime.timedelta(days=1)
+        
+    print("Completed to:")
+    print("\t", start.strftime("%d/%m/%Y"))
+    
+def print_data(data):
+    for k in data.keys():
+        print(k)
+        # for v in data[k]:
+        #     print('\t', v)
 
 if __name__ == "__main__":
-    es_adapter_demo()
+    mexico_adapter_demo()
